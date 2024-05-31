a683eda82e6f228e396ccd6ca274b9079db7563f
skihyeon <hero981001@gmail.com>
2024-05-31 13:42:19 +0900

    fix: IoU 계산을 gpu에서 가능하게

--- cal_IoU.py
+++ cal_IoU.py
@@ -1,14 +1,14 @@
-import numpy as np
+import cupy as cp
 from shapely.geometry import Polygon
 
 def area_of_union(det_x, det_y, gt_x, gt_y):
-    p1 = Polygon(np.column_stack((det_x, det_y))).buffer(0)
-    p2 = Polygon(np.column_stack((gt_x, gt_y))).buffer(0)
+    p1 = Polygon(cp.asnumpy(cp.column_stack((det_x, det_y)))).buffer(0)
+    p2 = Polygon(cp.asnumpy(cp.column_stack((gt_x, gt_y)))).buffer(0)
     return float(p1.union(p2).area)
 
 def area_of_intersection(det_x, det_y, gt_x, gt_y):
-    p1 = Polygon(np.column_stack((det_x, det_y))).buffer(0)
-    p2 = Polygon(np.column_stack((gt_x, gt_y))).buffer(0)
+    p1 = Polygon(cp.asnumpy(cp.column_stack((det_x, det_y)))).buffer(0)
+    p2 = Polygon(cp.asnumpy(cp.column_stack((gt_x, gt_y)))).buffer(0)
     return float(p1.intersection(p2).area)
 
 def iou(det_x, det_y, gt_x, gt_y):
@@ -20,11 +20,11 @@ def iou(det_x, det_y, gt_x, gt_y):
 
 def evaluate_iou(pred_contours, gt_contours):
     from scipy.optimize import linear_sum_assignment
-    print(f"{len(pred_contours)}/{len(gt_contours)} , {len(pred_contours/len(gt_contours)):.2f}% matched")
-    iou_matrix = np.zeros((len(gt_contours), len(pred_contours)))
+    print(f"{len(pred_contours)}/{len(gt_contours)} , {len(pred_contours)/len(gt_contours):.2f}% matched")
+    iou_matrix = cp.zeros((len(gt_contours), len(pred_contours)))
     for i, gt in enumerate(gt_contours):
         for j, pred in enumerate(pred_contours):
-            iou_matrix[i, j] = iou(pred[:, 0], pred[:, 1], gt[:, 0], gt[:, 1])
-    row_ind, col_ind = linear_sum_assignment(-iou_matrix)
+            iou_matrix[i, j] = iou(cp.asarray(pred[:, 0]), cp.asarray(pred[:, 1]), cp.asarray(gt[:, 0]), cp.asarray(gt[:, 1]))
+    row_ind, col_ind = linear_sum_assignment(cp.asnumpy(-iou_matrix))
     iou_scores = iou_matrix[row_ind, col_ind]
-    return iou_scores
+    return cp.asnumpy(iou_scores)
