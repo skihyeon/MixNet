518b01281721445d12975fd916df93647ec3d2fc
skihyeon <hero981001@gmail.com>
2024-05-31 13:49:05 +0900

    fix: debug

--- cal_IoU.py
+++ cal_IoU.py
@@ -20,7 +20,7 @@ def iou(det_x, det_y, gt_x, gt_y):
 
 def evaluate_iou(pred_contours, gt_contours):
     from scipy.optimize import linear_sum_assignment
-    print(f"{len(pred_contours)}/{len(gt_contours)} , {len(pred_contours/len(gt_contours)):.2f}% matched")
+    print(f"{len(pred_contours)}/{len(gt_contours)} , {100 * len(pred_contours)/len(gt_contours):.2f}% matched")
     iou_matrix = np.zeros((len(gt_contours), len(pred_contours)))
     for i, gt in enumerate(gt_contours):
         for j, pred in enumerate(pred_contours):
