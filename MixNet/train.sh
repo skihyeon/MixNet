9b55d2e259c64e59ae5c0c4fdc4d7e05f57d04ea
skihyeon <hero981001@gmail.com>
2024-07-01 07:05:13 +0000

    fix: no use accumulation

--- MixNet/train.sh
+++ MixNet/train.sh
@@ -1,3 +1,2 @@
 accelerate launch train.py --exp_name noaug_all \
---lr 1e-3 --batch_size 2 --start_epoch 1 --num_workers 48 --load_memory True
-# --input_size 1024
+--lr 1e-3 --batch_size 16 --start_epoch 1 --num_workers 12 --load_memory True --input_size 1024
